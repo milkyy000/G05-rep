ed0b828729ba421333bc71afb3a2dd0010ffd991
Quy Duc <s4070049@rmit.edu.vn>
2024-08-29 11:18:36 +0700

    add trending page and newposts

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://BeNa:FSG5123-Admin@fsg5.myx06.mongodb.net/?retryWrites=true&w=majority&appName=FSG5')
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch((error) => console.log(error.message));
+const app = express();
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    userName: {type: String, require: true, unique: true},
+    description: {type: String},
+    profilePic: {type: String},
+    createAt: {type: Date, default: Date.now}
+});
+
+const user = mongoose.model('user',userSchema);
+module.exports = user;
