b1f945f5391d168b2949a131cb04f471dd3b0410
Bao Toan <s4045102@rmit.edu.vn>
2024-08-30 10:38:39 +0700

    adding display profile with login test

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
+const User = require("./models/user"); // Import the User model from user.js 
 
 // Replace <yourusername> and <password> with your username and actual password for the mongodb atlas cluster
-mongoose.connect('mongodb+srv://BeNa:FSG5123-Admin@fsg5.myx06.mongodb.net/?retryWrites=true&w=majority&appName=FSG5')
+mongoose.connect('mongodb+srv://BeNa:FSG5123-Admin@fsg5.myx06.mongodb.net/Na_test_FSG5?retryWrites=true&w=majority&appName=FSG5')
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch((error) => console.log(error.message));
+
+// const newUser = new User({
+//     name: "Na",
+//     pfp: "neco-arc.png",
+//     status: "Straight jorkin it",
+//     description: "May thy jorketh' thee meat"
+// });
+
+// newUser.save()
+// .then(user => {
+//     console.log("User created: ", user);
+//     mongoose.connection.close();
+// })
+// .catch(err => {
+//     console.log("User not created", err);
+//     mongoose.connection.close();
+// });
